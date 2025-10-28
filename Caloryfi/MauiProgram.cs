71b742bb5612e4396191919018362318714d4797
Dawid Mackiewicz <dawid2004m@gmail.com>
2025-10-28 12:33:03 +0100

    Add project files.

---
+++ Caloryfi/MauiProgram.cs
@@ -0,0 +1,56 @@
+﻿using CommunityToolkit.Maui;
+using Microsoft.Extensions.Logging;
+using System.Net.Http.Headers;
+
+namespace Caloryfi
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .UseMauiCommunityToolkit()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+
+            //API CONNECTION SETTINGS
+            builder.Services.AddSingleton<HttpClient>(serviceProvider =>
+            {
+                var apiBaseUrl = "https://5e79fd12faf5.ngrok-free.app";
+
+                var httpClient = new HttpClient()
+                {
+                    BaseAddress = new Uri(apiBaseUrl),
+                    Timeout = TimeSpan.FromSeconds(30)
+                };
+
+                // Konfiguracja nagłówków
+                httpClient.DefaultRequestHeaders.Accept.Clear();
+                httpClient.DefaultRequestHeaders.Accept.Add(
+                    new MediaTypeWithQualityHeaderValue("application/json"));
+
+                return httpClient;
+            });
+
+            //APPSHELL
+            builder.Services.AddSingleton<AppShell>();
+
+            //SERVICE REGISTRATIONS
+
+            //VIEW
+
+            //VIEWMODEL
+
+#if DEBUG
+            builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
